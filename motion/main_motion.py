27a93f305c6c31b00e0d6c204cbd0a2c07569f57
anhtu duong <anhtu.duong@studenti.unitn.it>
2023-06-21 00:27:14 +0200

    gripper fix

--- motion/main_motion.py
+++ motion/main_motion.py
@@ -11,6 +11,7 @@ if str(ROOT) not in sys.path:
 # Import
 import rospy as ros
 import locosim.robot_control.base_controllers.params as conf
+import numpy as np
 from motion.utils import *
 from utils_ur5.Logger import Logger as log
 
@@ -19,6 +20,7 @@ from geometry_msgs.msg import Pose
 from ros_impedance_controller.srv import MoveJoints, MoveJointsRequest, MoveJointsResponse
 from ros_impedance_controller.srv import MoveTo, MoveToRequest, MoveToResponse
 from ros_impedance_controller.srv import generic_float
+from sensor_msgs.msg import JointState
 
 class Motion():
     """
@@ -32,6 +34,7 @@ class Motion():
 
         # Init variables
         self.robot_name = 'ur5'
+        self.joint_names = conf.robot_params[self.robot_name]['joint_names']
         self.dt = conf.robot_params[self.robot_name]['dt']
         self.rate = ros.Rate(1 / self.dt)
         self.real_robot = conf.robot_params[self.robot_name]['real_robot']
@@ -40,13 +43,50 @@ class Motion():
         else:
             self.v_des = 0.6
 
+        self.q = np.zeros(len(self.joint_names))
+        self.qd = np.zeros(len(self.joint_names))
+        self.tau = np.zeros(len(self.joint_names))
+
+
+        # Ros subscribers
+        self.joint_states_sub = ros.Subscriber('/joint_states', JointState, self.joint_states_callback)
+
         # Ros service clients
         ros.wait_for_service('/ur5/move_joints')
         self.move_joints_srv = ros.ServiceProxy('/ur5/move_joints', MoveJoints)
         ros.wait_for_service('/ur5/move_to')
         self.move_to_srv = ros.ServiceProxy('/ur5/move_to', MoveTo)
         ros.wait_for_service('move_gripper')
-        self.move_gripper_srv = ros.ServiceProxy('move_gripper', generic_float)
+        self.move_gripper_srv = ros.ServiceProxy('/ur5/move_gripper', generic_float)
+
+    def joint_states_callback(self, msg):
+        """
+        """
+        for msg_idx in range(len(msg.name)):          
+            for joint_idx in range(len(self.joint_names)):
+                if self.joint_names[joint_idx] == msg.name[msg_idx]: 
+                    self.q[joint_idx] = msg.position[msg_idx]
+                    self.qd[joint_idx] = msg.velocity[msg_idx]
+                    self.tau[joint_idx] = msg.effort[msg_idx]
+
+    def move_joints(self, q_des):
+        """
+        """
+        # Create a request object for MoveJoints
+        req = MoveJointsRequest()
+        req.q_des = q_des
+        req.dt = self.dt
+        req.v_des = self.v_des
+
+        # Call the service
+        log.debug_highlight('Start movement')
+        res = self.move_joints_srv(req)
+
+        if res.success:
+            log.info(f'Movement succeeded')
+        else:
+            log.error(f'Movement failed')
+        return res
         
     def move_to(self, pose_target):
         """
@@ -74,10 +114,10 @@ class Motion():
             log.error(f'Movement failed')
         return res
 
-    def gripper_move(self, diameter):
+    def move_gripper(self, diameter):
         """
         """
-        log.debug_highlight(f'Start gripper, diameter = {diameter}')
+        log.debug_highlight(f'Start gripper')
         # Create a request object
         request = generic_float._request_class()
         request.data = diameter
@@ -85,7 +125,11 @@ class Motion():
         # Send the request to the service
         response = self.move_gripper_srv(request)
 
-        log.debug(f'response = {response}')
+        if response:
+            log.info(f'Gripper command sent: {diameter}')
+        else:
+            log.error(f'Gripper command failed: {diameter}')
+
     
 
     def run(self):
@@ -107,13 +151,18 @@ class Motion():
         #     p.move_joints(p.dt, p.v_des, pose, rate)
 
         # p.move_joints(p.dt, p.v_des, p.q_guess['pick'], rate)
-        pose_target.position.z += 0.1
-        self.move_to(pose_target)
+        # pose_target.position.z += 0.1
+        # self.move_to(pose_target)
 
-        pose_target.position.z -= 0.1
-        self.move_to(pose_target)
+        # pose_target.position.z -= 0.1
+        # self.move_to(pose_target)
 
-        self.move_gripper_srv(30)
+        self.move_gripper(30)
+
+        # pose_target.position.z += 0.1
+        # self.move_to(pose_target)
+
+        self.move_gripper(60)
         
 
         self.rate.sleep()
